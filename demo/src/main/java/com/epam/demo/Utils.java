1109bdd3652dc01b0ef52474e672be96539adf48
Azimbek <omar.azimbek@gmail.com>
2024-01-24 14:39:06 +0600

    2.1 Commit 4

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,12 +4,22 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        int counter = 0;
-        for (String s: args) {
-            if (Integer.parseInt(s)>0) {
-                counter++;
+        if (args.isEmpty() || args==null) {
+            return false;
+        }
+        for (String s : args) {
+            if (s.isEmpty() || s==null) {
+                return false;
+            }
+            try {
+                if (Double.parseDouble(s)<=0) {
+                    return false;
+                }
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                return false;
             }
         }
-        return counter==args.size();
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,7 +4,16 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         return Integer.parseInt(str)>0;
+         if (str.isEmpty() || str==null) {
+             return false;
+         }
+         try {
+             return Double.parseDouble(str)>0;
+         }
+         catch (NumberFormatException e) {
+             e.printStackTrace();
+             return false;
+         }
     }
     public static void main(String[] args) {
         System.out.println(isPositiveNumber("1"));
