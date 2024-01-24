757b42d9d52a7d5d5b9aadbe66b2f6e5f09a5e9e
Azimbek <omar.azimbek@gmail.com>
2024-01-24 12:54:52 +0600

    2.1 Commit

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,9 +1,12 @@
 package com.epam.utils;
 
-import org.apache.commons.lang3.StringUtils;
+//import org.apache.commons.lang3.StringUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          return Integer.parseInt(str)>0;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPositiveNumber("1"));
     }
 }
